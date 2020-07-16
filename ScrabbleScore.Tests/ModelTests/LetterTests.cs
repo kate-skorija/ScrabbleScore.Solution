b36907104e589220736b13872a918e938f5c13f8
Kate Skorija <kateskorija@gmail.com>
2020-07-16 10:31:30 -0700

    create passing Constructor test for Letter class
    Co-Authored-By: Chris Yoon <chy.yoon@gmail.com>

--- ScrabbleScore.Tests/ModelTests/LetterTests.cs
+++ ScrabbleScore.Tests/ModelTests/LetterTests.cs
@@ -1,3 +1,4 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using ScrabbleScore.Models;
 
--- ScrabbleScore/Models/Letter.cs
+++ ScrabbleScore/Models/Letter.cs
@@ -8,3 +8,23 @@ namespace ScrabbleScore.Models
 
   }
 }
+
+//constructor with inputword argument
+//method that puts word into char array
+//another method? that iterates thru array and assign points
+//method to getpoints?
+
+// for(int x in charArray)
+// if (chararray[x].ToMatch(regex[$a\f\d\g\s)i+g)
+// {
+//   counter += 1;
+// }
+// else if(chararray[x].ToMatch(regex[d\f\d\g\s))
+
+//
+// Dictionary<string, int> ScrabbleLetters = new Dictionary<string,int>(){{"A", 1}, {"B", 3}, {"C", 3}, {"D", 2}, {"E", 1}, {"F", 4}, {"G", 2}, {"H", 4}, {"I", 1}, {"J", 8}, {"K", 5}, {"L", 1}, {"M", 3}, {"N", 1}, {"O", 1}, {"P", 3}, {"Q", 10}, {"R", 1}, {"S", 1}, {"T", 1}, {"U", 1}, {"V", 4}, {"W", 4}, {"X", 8}, {"Y", 4}, {"Z", 10}}
+//foreach (int x in charArray)
+//{
+//  counter += ScrabbleLetter[x]
+//}
+// return counter
